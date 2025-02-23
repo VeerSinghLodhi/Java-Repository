d81e4b463b985a3dfcda78c944a3b0f94dc5601d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-02-23 22:04:04 +0530

    Add files via upload

---
+++ firstproject/CollegeStudent.java
@@ -0,0 +1,15 @@
+package firstproject;
+
+
+public class CollegeStudent extends Student {
+    String department;
+    
+    void setCollegeStudent(String department)
+    {
+        this.department=department;
+    }
+    void getCollegeStudent()
+    {
+        System.out.println("Department is "+department);
+    }
+}
---
+++ firstproject/School_Student.java
@@ -0,0 +1,17 @@
+package firstproject;
+
+
+public class School_Student extends Student {
+    
+    String Class ;
+    
+    void setSchool_Student(String Class)
+    {
+        this.Class=Class;
+    }
+    void getSchool_Student()
+    {
+        System.out.println("Studying in "+Class);
+    }
+    
+}
