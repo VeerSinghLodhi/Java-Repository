f0be3708ce020646114eba1a85c4edc7f2796018
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-18 20:01:16 +0530

    Add files via upload

---
+++ LamdaExpression/SumInter.java
@@ -0,0 +1,6 @@
+
+// Functional Interface : if Interface has only one abstract method. like SumInter.
+@FunctionalInterface
+public interface SumInter {
+    public abstract int sum(int a,int c);
+}
